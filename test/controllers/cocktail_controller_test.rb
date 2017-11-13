059c9ec351cbeff3ee6dc7b31e1eca9d0b22c676
Andrew Nagle <annagle@tcd.ie>
2017-11-13 13:59:12 +0000

    cocktails

---
+++ test/controllers/cocktail_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CocktailControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get cocktail_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get cocktail_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get cocktail_new_url
+    assert_response :success
+  end
+
+end
